5fae5d788d9f7f37d2df37b26a4f542ab4a9ac12
ntxm <51963319+ntxm@users.noreply.github.com>
2020-01-20 00:56:08 -0500

    properties file added and testcases

---
+++ src/test/java/com/practice/LoginPropertiesFileTest.java
@@ -0,0 +1,43 @@
+package com.practice;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+import com.hrms.pages.LoginPageElements;
+import com.hrms.utils.CommonMethods;
+
+public class LoginPropertiesFileTest extends CommonMethods {
+	
+	Properties property;
+	
+	@Test
+	public void login() throws InterruptedException {
+		
+		String filePath = System.getProperty("user.dir")+ "/src/test/java/com/practice/settings.properties";
+		try {
+			FileInputStream fis = new FileInputStream(filePath);
+			property = new Properties();
+			property.load(fis);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			
+		}
+		
+		//String loginUsername = ;
+		LoginPageElements login = new LoginPageElements();
+		sendText(login.username, property.getProperty("username"));
+		sendText(login.password, property.getProperty("password"));
+		jsClick(login.loginBtn);
+		
+		Thread.sleep(3000);
+		
+		
+	}
+	
+
+}
