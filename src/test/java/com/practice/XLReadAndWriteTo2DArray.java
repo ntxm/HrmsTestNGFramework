ed0129fce5560e0479e02c9fa99d208d3535216e
ntxm <51963319+ntxm@users.noreply.github.com>
2020-01-22 03:00:44 -0500

    XL reader added

--- src/test/java/com/practice/XLReadAndWriteTo2DArray.java
+++ src/test/java/com/practice/XLReadAndWriteTo2DArray.java
@@ -4,14 +4,13 @@ import java.io.FileInputStream;
 
 import java.io.IOException;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
 
-public class XLReader {
+public class XLReadAndWriteTo2DArray {
 	
 	@Test
 	public void read() throws IOException {
@@ -27,10 +26,32 @@ public class XLReader {
 		//get the sheet which you want to modify or create
 		Sheet sheet = wbook.getSheet("Sheet1");
 		
-		Row row = sheet.getRow(0);
-		Cell cell = row.getCell(0);
-		String value = cell.toString();
-		System.out.println(value);
+		//Row row = sheet.getRow(0);
+		//Cell cell = row.getCell(0);
+		//String value = cell.toString();
+		
+		//get number of rows
+		int rows = sheet.getPhysicalNumberOfRows();
+		//get number of cols
+		int cols = sheet.getRow(0).getLastCellNum();
+		//create 2D array with rows and cols size
+		String[][] array = new String[rows][cols];
+		//loop through each row and column
+		for(int i = 0; i < rows; i++) {
+			for(int y = 0; y < cols; y++) {
+				//access value at specified location
+				String cellValue = sheet.getRow(i).getCell(y).toString();
+				//add to 2d array
+				array[i][y] = cellValue;
+				
+				
+				System.out.println(array[i][y]);
+				
+			}
+			System.out.println(" ");
+		}
+		
 		
 	}
 }
+
